1e33217a3ffbbb761b2a3a0ab2e0f98b38d96650
ghalia543 <ghaliahamed453@gmail.com>
2025-05-12 16:42:26 +0300

    Update System.h

--- System.h
+++ System.h
@@ -2,14 +2,15 @@
 #include "UserManager.h"
 #include <string>
 #include <unordered_map>
+#include "User.h"
 
-class System
-{
-
+class System {
+private:
+    User* current_LoggedIN_user;  
 public:
     System();
-    bool registerUser(const string& uname, const string& pass);
-    bool login(const string& username, const string& password);
-
+    bool registerUser(const std::string& uname, const std::string& pass);
+    bool login(const std::string& username, const std::string& password);
+    User& getCurrent_LoggedIN_user();
+    void setCurrent_LoggedIN_user(User& user);
 };
-
