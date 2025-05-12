9750e3ab8383af6f8dd03bc3248344e5a953d048
farahh2004 <farahhhmohamed9990@gmail.com>
2025-05-12 19:07:56 +0300

    Update System.h

--- System.h
+++ System.h
@@ -6,11 +6,12 @@
 
 class System {
 private:
-    User* current_LoggedIN_user;  
+    User* current_LoggedIN_user;
 public:
     System();
     bool registerUser(const std::string& uname, const std::string& pass);
     bool login(const std::string& username, const std::string& password);
     User& getCurrent_LoggedIN_user();
     void setCurrent_LoggedIN_user(User& user);
+    void changeUserPassword(const string& userId, const string& newPassword);
 };
