7aa9f04c91604bbc8cb3fc4b60fcfee33c9aa88b
Toaa EmadEldin Muhammad <166917089+ToaaEmad@users.noreply.github.com>
2025-05-13 10:19:08 +0300

    Ufinal update UserManager.h

--- UserManager.h
+++ UserManager.h
@@ -11,17 +11,12 @@ class UserManager
 private:
 	const string UserFilePath = "users.txt";
 	static unordered_map <string, User> users;
-	static unordered_map<string, string> idToUsername;
 
 public:
 	UserManager();
 
 	bool loadUsersFromFile();
 	bool saveUsersToFile();
-	/*unordered_map <string, User> return_user() {
-		return users;
-	}*/
 	static void addUser(string uname, User& newUser);
 	static User& searchUser(string uname);
-	static User& searchUserById(string uid);
 };
