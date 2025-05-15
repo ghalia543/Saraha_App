f160648956f4d1775576d076aaedc4546a5b466c
farahh2004 <farahhhmohamed9990@gmail.com>
2025-05-15 18:20:03 +0300

    Update Message.h

--- Message.h
+++ Message.h
@@ -21,29 +21,29 @@ public:
 
     //Constructors
     Message();
-    Message(string from, string to,  string text);
+    Message(string from, string to, string text);
     Message(string messID, string from, string to, string text);
-    
+
     // Getters
     string getMessageId();
-    string getSenderId() ;
-    string getReceiverUsername() ;
-    string getContent() ;
-    bool getIsFavorite() ;
-    string getSentTime() ; // return formatted time string
+    string getSenderId();
+    string getReceiverUsername();
+    string getContent();
+    bool getIsFavorite();
+    string getSentTime(); // return formatted time string
 
     // Setters
     void setautoId(int nextId);
     void setMessageId(string id);
     void setSenderId(string id);
     void setReceiverUsername(string id);
-    void setContent( string& text);
+    void setContent(string& text);
     void setIsFavorite(bool favorite);
     void setTime();
     void setTime(string time);
 
     // Functions
-    void displayMessage() ;
+    void displayMessage();
 };
 
 #endif // MESSAGE_H
